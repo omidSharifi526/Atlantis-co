72b3e707a6dc363b0a8581750bd1876ac5d1b337
sharifidavood <sharifidavood743@gmail.com>
2022-01-12 03:11:00 -0800

    test commit

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>Atlantis-co</title>
-        <meta name="Atlantis-co" content="best of Hookah service" />
+        <meta name="Atlantis-co" content="best of Hookah service and hookah" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous"></link>
       </Head>
