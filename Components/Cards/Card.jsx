a18d6d63f4a44b97592f0d4d197e23a7aff8567d
sharifidavood <sharifidavood743@gmail.com>
2022-02-06 09:55:33 -0800

    Add-p-popular-row

--- Components/Cards/Card.jsx
+++ Components/Cards/Card.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 const Card = ({item,index}) => {
     //console.log(item);
   return <div>
-      <div class="card mcard text-center bg-dark text-white" >
+      <div className="card mcard text-center bg-dark text-white" >
   <img src={item.img} className="card-img-top mb-2 w-100 " alt="..."/>
   <div className="card-body">
     <h5 className="card-title">قلیان ویژه</h5>
     <p className="card-text p-2 ">لورم اپسون متن ساختگی</p>
-    <a href="#" class="btn btn-danger">خرید</a>
+    <a href="#" className="btn btn-danger">خرید</a>
   </div>
 </div>
   </div>;
